8b2e02eb436ec2381ff3d9f51021d2cd74eb348b
miku561 <673391502@qq.com>
2017-02-14 19:25:52 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function(){
+    $("#dowebok").fullpage({
+        navigation: true,   
+        navigationColor: "#ff0000",
+        css3: true,
+        sectionsColor:["#748B9D","#748B9D","#748B9D","#748B9D"]
+    });
+});
